707d9f2c327e727cfa14afe10ab97ef9b4997e34
andrew-geng <gpeng1986@gmail.com>
2021-06-23 22:01:10 +0800

    1

--- src/java/com/martix/x/pub/list/RemovedDuplicates2Solution.java
+++ src/java/com/martix/x/pub/list/RemovedDuplicates2Solution.java
@@ -16,6 +16,28 @@ public class RemovedDuplicates2Solution {
 
     public ListNode deleteDuplicates(ListNode head) {
         if (head == null) {
+            return head;
+        }
+
+        ListNode dummy = new ListNode(0, head);
+
+        ListNode cur = dummy;
+        while (cur.next != null && cur.next.next != null) {
+            if (cur.next.val == cur.next.next.val) {
+                int x = cur.next.val;
+                while (cur.next != null && cur.next.val == x) {
+                    cur.next = cur.next.next;
+                }
+            } else {
+                cur = cur.next;
+            }
+        }
+
+        return dummy.next;
+    }
+
+    public ListNode deleteDuplicates_0(ListNode head) {
+        if (head == null) {
             return null;
         }
 
