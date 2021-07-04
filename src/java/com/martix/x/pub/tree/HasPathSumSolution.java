b0e1a3c4c9200f8ec4b173efcdadd6498b645ef3
andrew-geng <gpeng1986@gmail.com>
2021-07-04 14:18:23 +0800

    1

--- src/java/com/martix/x/pub/tree/HasPathSumSolution.java
+++ src/java/com/martix/x/pub/tree/HasPathSumSolution.java
@@ -4,7 +4,8 @@ package com.martix.x.pub.tree;
  * Created by Andrew-Geng on 11:58 下午 2021/5/15
  * 路径总和 lc 112
  * <p>
- * 给你二叉树的根节点 root 和一个表示目标和的整数 targetSum ，判断该树中是否存在 根节点到叶子节点 的路径，这条路径上所有节点值相加等于目标和 targetSum 。
+ * 给你二叉树的根节点root 和一个表示目标和的整数targetSum ，判断该树中是否存在根节点到叶子节点的路径，
+ * 这条路径上所有节点值相加等于目标和targetSum 。
  * <p>
  * 叶子节点 是指没有子节点的节点。
  * <p>
