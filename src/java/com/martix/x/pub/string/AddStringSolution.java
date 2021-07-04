e90b6b0a8efac76d48f39654d54903e6c6236a57
andrew-geng <gpeng1986@gmail.com>
2021-07-04 15:15:14 +0800

    1

--- src/java/com/martix/x/pub/string/AddStringSolution.java
+++ src/java/com/martix/x/pub/string/AddStringSolution.java
@@ -4,16 +4,14 @@ package com.martix.x.pub.string;
  * Created by Andrew-Geng on 12:00 上午 2021/6/28
  *
  * 字符串相加 lc 415
- * 给定两个字符串形式的非负整数 num1 和num2 ，计算它们的和。
- *
- *  
+ * 给定两个字符串形式的非负整数num1 和num2，计算它们的和。
  *
  * 提示：
  *
- * num1 和num2 的长度都小于 5100
- * num1 和num2 都只包含数字 0-9
+ * num1 和num2的长度都小于 5100
+ * num1 和num2 都只包含数字0-9
  * num1 和num2 都不包含任何前导零
- * 你不能使用任何內建 BigInteger 库， 也不能直接将输入的字符串转换为整数形式
+ * 你不能使用任何內建 BigInteger 库，也不能直接将输入的字符串转换为整数形式
  *
  */
 public class AddStringSolution {
