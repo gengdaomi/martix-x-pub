46258caa1cfeb8df536629ac4cac6e9f46205b1f
andrew-geng <gpeng1986@gmail.com>
2021-07-13 22:28:51 +0800

    1

--- src/java/com/martix/x/pub/set/SubSetSolution.java
+++ src/java/com/martix/x/pub/set/SubSetSolution.java
@@ -64,4 +64,27 @@ public class SubSetSolution {
         visited[index] = false;
         robot(index + 1, nums);
     }
+
+
+    /**
+     * 回溯算法
+     * @param nums
+     * @return
+     */
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        backtrack(0, nums, res, new ArrayList<Integer>());
+        return res;
+
+    }
+
+    private void backtrack(int i, int[] nums, List<List<Integer>> res, ArrayList<Integer> tmp) {
+        res.add(new ArrayList<>(tmp));
+        for (int j = i; j < nums.length; j++) {
+            tmp.add(nums[j]);
+            backtrack(j + 1, nums, res, tmp);
+            tmp.remove(tmp.size() - 1);
+        }
+    }
+
 }
