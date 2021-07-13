b5c22648b642082016c94e9bf849a33441236fe7
andrew-geng <gpeng1986@gmail.com>
2021-07-14 00:59:03 +0800

    1

--- src/java/com/martix/x/pub/merge/RangeInsertSolution.java
+++ src/java/com/martix/x/pub/merge/RangeInsertSolution.java
@@ -10,6 +10,7 @@ import java.util.stream.Stream;
 /**
  * Created by ayue on 下午3:07 2018/7/6
  * 给出一个无重叠的 ，按照区间起始端点排序的区间列表。
+ * lc 57
  * <p>
  * 在列表中插入一个新的区间，你需要确保列表中的区间仍然有序且不重叠（如果有必要的话，可以合并区间）。
  * <p>
@@ -36,41 +37,49 @@ public class RangeInsertSolution {
 
     }
 
+    /**
+     * 时间复杂度：O(n)，其中n 是数组 intervals 的长度，即给定的区间个数;
+     * 空间复杂度：O(1)。除了存储返回答案的空间以外，我们只需要额外的常数空间即可
+     *
+     * @param intervals
+     * @param newInterval
+     * @return
+     */
     public int[][] insert(int[][] intervals, int[] newInterval) {
         int left = newInterval[0];
         int right = newInterval[1];
 
         boolean placed = false;
-        List<int[]> ansList = new ArrayList<>();
+        List<int[]> result = new ArrayList<>();
 
         for (int[] interval : intervals) {
-            if (interval[0] > right) {
-                // 在插入区间的右侧且无交集
+            if (interval[0] > right) {// 在插入区间的右侧且无交集
                 if (!placed) {
-                    ansList.add(new int[]{left, right});
+
+                    result.add(new int[]{left, right});
                     placed = true;
                 }
-                ansList.add(interval);
-            } else if (interval[1] < left) {
-                // 在插入区间的左侧且无交集
-                ansList.add(interval);
-            } else {
-                // 与插入区间有交集，计算它们的并集
+
+                result.add(interval);
+            } else if (interval[1] < left) {// 在插入区间的左侧且无交集
+                result.add(interval);
+            } else {// 与插入区间有交集，计算它们的并集
+
                 left = Math.min(left, interval[0]);
                 right = Math.max(right, interval[1]);
             }
         }
 
         if (!placed) {
-            ansList.add(new int[]{left, right});
+            result.add(new int[]{left, right});
         }
 
-        int[][] result = new int[ansList.size()][2];
-        for (int i = 0; i < ansList.size(); ++i) {
-            result[i] = ansList.get(i);
+        int[][] resultArr = new int[result.size()][2];
+        for (int i = 0; i < result.size(); ++i) {
+            resultArr[i] = result.get(i);
         }
 
-        return result;
+        return resultArr;
     }
 
     public int[][] insert_1(int[][] intervals, int[] newInterval) {
