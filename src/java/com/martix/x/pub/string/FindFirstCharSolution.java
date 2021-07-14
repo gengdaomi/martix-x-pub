365545d8a393ea4394931b7336ec26f1ef8aec30
andrew-geng <gpeng1986@gmail.com>
2021-07-14 20:12:12 +0800

    1

--- src/java/com/martix/x/pub/string/FindFirstCharSolution.java
+++ src/java/com/martix/x/pub/string/FindFirstCharSolution.java
@@ -5,7 +5,7 @@ package com.martix.x.pub.string;
  * 1: 给定任意一个字符串，找到这个字符串当中，第一次出现且仅出现一次的字符。 如：“abac”,输出b，“abbacdc”，输出d，若无法找到，则输出“cannot find char”。
  * Created By Andrew-Geng on 2020/8/28 8:43 下午
  */
-public class FindFirstSolution {
+public class FindFirstCharSolution {
 
     public String execute(String str) {
         int[] count = new int[26];
