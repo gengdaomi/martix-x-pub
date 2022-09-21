e412f6d0596e3277d0210354ef6d9b7349608208
gengpeng <>
2022-09-21 13:19:07 +0800

    1

--- src/java/com/martix/x/pub/code/string/ZConvertSolution.java
+++ src/java/com/martix/x/pub/code/string/ZConvertSolution.java
@@ -8,9 +8,9 @@ import java.util.List;
  * Z 字形变换
  * lc 6
  * <p>
- * 将一个给定字符串 s 根据给定的行数 numRows ，以从上往下、从左到右进行 Z 字形排列。
+ * 将一个给定字符串 s 根据给定的行数 numRows ，以从上往下、从左到右进行Z 字形排列。
  * <p>
- * 比如输入字符串为 "PAYPALISHIRING" 行数为 3 时，排列如下：
+ * 比如输入字符串为 "PAYPALISHIRING"行数为 3 时，排列如下：
  * <p>
  * P   A   H   N
  * A P L S I I G
