574fd256221a7eb6ea05902566aff5ac8dafc0bc
gengpeng <>
2022-09-23 16:03:43 +0800

    11

--- src/java/com/martix/x/pub/code/palindrome/SplitPalindromeLeastCountSolution.java
+++ src/java/com/martix/x/pub/code/palindrome/SplitPalindromeLeastCountSolution.java
@@ -12,13 +12,12 @@ import java.util.Arrays;
  *
  * 返回符合要求的 最少分割次数 。
  *
- *  
  *
  * 示例 1：
  *
  * 输入：s = "aab"
  * 输出：1
- * 解释：只需一次分割就可将 s 分割成 ["aa","b"] 这样两个回文子串。
+ * 解释：只需一次分割就可将s 分割成 ["aa","b"] 这样两个回文子串。
  * 示例 2：
  *
  * 输入：s = "a"
@@ -32,7 +31,7 @@ import java.util.Arrays;
  * 链接：https://leetcode-cn.com/problems/palindrome-partitioning-ii
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
-public class SplitPalindrome2Solution {
+public class SplitPalindromeLeastCountSolution {
 
     /**
      * 动态规划
