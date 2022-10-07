db6e2a0d56cf31fe64a026966cc3dababf7370b1
gengpeng <>
2022-10-07 14:27:37 +0800

    1

--- src/java/com/martix/x/pub/code/prefix/SubArraySumNKValidateSolution.java
+++ src/java/com/martix/x/pub/code/prefix/SubArraySumNKValidateSolution.java
@@ -1,4 +1,4 @@
-package com.martix.x.pub.code.arrays;
+package com.martix.x.pub.code.prefix;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -11,6 +11,8 @@ import java.util.Map;
  * 给定一个包含 非负数 的数组和一个目标 整数k ，编写一个函数来判断该数组是否含有连续的子数组，
  * 其大小至少为 2，且总和为 k 的倍数，即总和为 n * k ，其中 n 也是一个整数。
  * <p>
+ *  
+ * <p>
  * 示例 1：
  * <p>
  * 输入：[23,2,4,6,7], k = 6
@@ -21,17 +23,18 @@ import java.util.Map;
  * 输入：[23,2,6,4,7], k = 6
  * 输出：True
  * 解释：[23,2,6,4,7]是大小为 5 的子数组，并且和为 42。
+ *  
  * <p>
  * 说明：
  * <p>
  * 数组的长度不会超过 10,000 。
  * 你可以认为所有数字总和在 32 位有符号整数范围内。
  */
-public class SubArraySumValidateSolution {
+public class SubArraySumNKValidateSolution {
 
     public static void main(String[] args) {
         int[] num = new int[]{1, 1};
-        boolean f = new SubArraySumValidateSolution().checkSubarraySum(num, 1);
+        boolean f = new SubArraySumNKValidateSolution().checkSubarraySum(num, 1);
         System.out.println(f);
     }
 
