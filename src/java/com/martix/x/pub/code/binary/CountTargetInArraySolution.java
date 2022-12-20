187b84b96be214bc79446bc850ce03196812a9b7
andrew-geng <gpeng1986@gmail.com>
2022-12-20 23:41:07 +0800

    1

---
+++ src/java/com/martix/x/pub/code/binary/CountTargetInArraySolution.java
@@ -0,0 +1,60 @@
+package com.martix.x.pub.code.binary;
+
+/**
+ * Created by Andrew-Geng on 23:21 2022/12/20
+ *
+ * 在排序数组中查找数字 I
+ *
+ * 剑指 Offer 53
+ *
+ * 输入: nums = [5,7,7,8,8,10], target = 8
+ * 输出: 2
+ * 示例 2:
+ *
+ * 输入: nums = [5,7,7,8,8,10], target = 6
+ * 输出: 0
+ *
+ */
+public class CountTargetInArraySolution {
+
+    /**
+     * 二分查找
+     *
+     * 考虑target 在数组中出现的次数，其实我们要找的就是数组中「第一个等于target 的位置」
+     * （记为leftIdx）和「第一个大于target 的位置减一」（记为rightIdx）。
+     * 当target 在数组中存在时，target 在数组中出现的次数为 rightIdx−leftIdx+1
+     *
+     * 时间复杂度O(logn)
+     * 空间复杂度O(1)
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int search(int[] nums, int target) {
+        int leftIdx = binarySearch(nums, target, true);
+        int rightIdx = binarySearch(nums, target, false) - 1;
+
+        if (leftIdx <= rightIdx && rightIdx < nums.length && nums[leftIdx] == target && nums[rightIdx] == target) {
+            return rightIdx - leftIdx + 1;
+        }
+
+        return 0;
+    }
+
+    public int binarySearch(int[] nums, int target, boolean lower) {
+        int left = 0, right = nums.length - 1, result = nums.length;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            
+            if (nums[mid] > target || (lower && nums[mid] >= target)) {
+                right = mid - 1;
+                result = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return result;
+    }
+}
---
+++ src/java/com/martix/x/pub/code/string/RepeatedSubstringPatternSolution.java
@@ -0,0 +1,131 @@
+package com.martix.x.pub.code.string;
+
+import java.util.Arrays;
+
+/**
+ * Created by Andrew-Geng on 23:01 2022/12/20
+ * 重复的子字符串 lc 459
+ *
+ * 给定一个非空的字符串s，检查是否可以通过由它的一个子串重复多次构成。
+ *
+ *
+ * 示例 1:
+ *
+ * 输入: s = "abab"
+ * 输出: true
+ * 解释: 可由子串 "ab" 重复两次构成。
+ * 示例 2:
+ *
+ * 输入: s = "aba"
+ * 输出: false
+ * 示例 3:
+ *
+ * 输入: s = "abcabcabcabc"
+ * 输出: true
+ * 解释: 可由子串 "abc" 重复四次构成。 (或子串 "abcabc" 重复两次构成。)
+ */
+public class RepeatedSubstringPatternSolution {
+
+    /**
+     * 如果您的字符串 S 包含一个重复的子字符串，那么这意味着您可以多次 “移位和换行”`您的字符串，并使其与原始字符串匹配。
+     *
+     * 例如：abcabc
+     *
+     * 移位一次：cabcab
+     * 移位两次：bcabca
+     * 移位三次：abcabc
+     *
+     * 现在字符串和原字符串匹配了，所以可以得出结论存在重复的子串。
+     *
+     * 基于这个思想，可以每次移动k个字符，直到匹配移动 length - 1 次。但是这样对于重复字符串很长的字符串，效率会非常低。在 LeetCode 中执行时间超时了。
+     *
+     * 为了避免这种无用的环绕，可以创建一个新的字符串 str，它等于原来的字符串 S 再加上 S 自身，这样其实就包含了所有移动的字符串。
+     *
+     * 比如字符串：S = acd，那么 str = S + S = acdacd
+     *
+     * acd 移动的可能：dac、cda。其实都包含在了 str 中了。就像一个滑动窗口
+     *
+     * 一开始 acd (acd) ，移动一次 ac(dac)d，移动两次 a(cda)cd。循环结束
+     *
+     * 所以可以直接判断 str 中去除首尾元素之后，是否包含自身元素。如果包含。则表明存在重复子串
+
+     *
+     * @param s
+     * @return
+     */
+    public boolean repeatedSubstringPattern(String s) {
+        String str = s + s;
+        return str.substring(1, str.length() - 1).contains(s);
+    }
+
+    /**
+     * 枚举
+     *
+     * 如果一个长度为n 的字符串s 可以由它的一个长度为n，的子串s重复多次构成；
+     *
+     * 因此，我们可以从小到大枚举n′，并对字符串s 进行遍历，进行上述的判断。注意到一个小优化是，
+     * 因为子串至少需要重复一次，所以n′不会大于n 的一半，我们只需要在[1,n/2]的范围内枚举n即可。
+     *
+     * 时间复杂度O(n^2)
+     * 空间复杂度O(1)
+     * @param s
+     * @return
+     */
+    public boolean repeatedSubstringPattern_1(String s) {
+        int n = s.length();
+
+        for (int i = 1; i * 2 <= n; i++) {
+
+            if (n % i == 0) {
+                boolean match = true;
+
+                for (int j = i; j < n; j++) {
+                    if (s.charAt(j) != s.charAt(j - i)) {
+                        match = false;
+                        break;
+                    }
+
+                }
+
+                if (match) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * KMP算法
+     *
+     * 时间复杂度O(n)
+     * 空间复杂度O(n)
+     * @param s
+     * @return
+     */
+    public boolean repeatedSubstringPattern_3(String s) {
+        return kmp(s);
+    }
+
+    public boolean kmp(String pattern) {
+        int n = pattern.length();
+        int[] fail = new int[n];
+        Arrays.fill(fail, -1);
+
+        for (int i = 1; i < n; i++) {
+            int j = fail[i - 1];
+
+            while (j != -1 && pattern.charAt(j + 1) != pattern.charAt(i)) {
+                j = fail[j];
+            }
+
+            if (pattern.charAt(j + 1) == pattern.charAt(i)) {
+                fail[i] = j + 1;
+            }
+        }
+
+        return fail[n - 1] != -1 && n % (n - fail[n - 1] - 1) == 0;
+    }
+
+}
